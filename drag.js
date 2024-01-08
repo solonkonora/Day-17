639f67f4c1b6f98b87d4c72fffb337f2e7126748
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-01-08 07:29:55 -0800

    solving linter errors

--- drag.js
+++ drag.js
@@ -14,7 +14,7 @@ for (const empty of empties) {
 function dragStart () {
   this.className += ' hold'
   setTimeout(() => {
-    this.className = 'invisible'}, 0)
+    this.className = 'invisible' }, 0)
 }
 
 function dragEnd () {
